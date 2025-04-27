e4dcc3677ef8a142e0f3f8f422b780bc2e91b040
ahmad.mirza <ahmad.mirza@sarana-jaya.co.id>
2025-04-28 00:04:25 +0700

    ok

--- application/views/visiting/detail_v.php
+++ application/views/visiting/detail_v.php
@@ -5,7 +5,15 @@
 				<td align="center">Check-in Time <?=$data['CheckInTimeIndFmt']?></td>
 			</tr>
 			<tr class="align-text-top">
-				<td align="center"><img src="<?= base_url('assets/uploads/checkin/'.$data['FileCI']); ?>" height="200"></td>
+				<td align="center">
+					<?php
+						if($data['FileCI']=='') {
+							echo "- No Checkin Picture -";
+						} else {
+					?>
+					<img src="<?= base_url('assets/uploads/checkin/'.$data['FileCI']); ?>" height="200">
+					<?php }?>
+				</td>
 			</tr>
 		</table>
 	</div>
@@ -15,17 +23,25 @@
 				<td align="center">Check-out Time <?=$data['CheckOutTimeIndFmt']?></td>
 			</tr>
 			<tr class="align-text-top">
-				<td align="center"><img src="<?= base_url('assets/uploads/checkout/'.$data['FileCO']); ?>" height="200"></td>
+				<td align="center">
+					<?php
+						if($data['FileCO']=='') {
+							echo "- No Checkout Picture -";
+						} else {
+					?>
+					<img src="<?= base_url('assets/uploads/checkout/'.$data['FileCO']); ?>" height="200">
+					<?php }?>
+				</td>
 			</tr>
 		</table>
 	</div>
 </div>
 
 <div class="row">
-	<div class="col-md-12">
+	<div class="col-md-6">
         <table class="text-black table">
             <tr class="align-text-top">
-                <td width="100px">Host </td>
+                <td width="160px">Host </td>
                 <td width="5px">:</td>
                 <td class="font-weight-bold"><?=$data['HostName']?></td>
             </tr>
@@ -48,6 +64,40 @@
                 <td>Company Type</td>
                 <td>:</td>
                 <td class="font-weight-bold"><?=$data['SourceTypeName']?></td>
+            </tr>
+        </table>
+    </div>
+	<div class="col-md-6">
+        <table class="text-black table">
+            <tr class="align-text-top">
+                <td width="160px">Address </td>
+                <td width="5px">:</td>
+                <td class="font-weight-bold"><?=$data['HostName']?></td>
+            </tr>
+            <tr class="align-text-top">
+                <td>ID Card</td>
+                <td>:</td>
+                <td class="font-weight-bold"><?=$data['IDCard']?></td>
+            </tr>
+            <tr class="align-text-top">
+                <td>Phone</td>
+                <td>:</td>
+                <td class="font-weight-bold"><?=$data['PhoneNumber']?></td>
+            </tr>
+            <tr class="align-text-top">
+                <td>Email</td>
+                <td>:</td>
+                <td class="font-weight-bold"><?=$data['Email']?></td>
+            </tr>
+            <tr class="align-text-top">
+                <td>Purpose Visit</td>
+                <td>:</td>
+                <td class="font-weight-bold"><?=$data['PurposeVisit']?></td>
+            </tr>
+			<tr class="align-text-top">
+                <td>Notes</td>
+                <td>:</td>
+                <td class="font-weight-bold"><?=$data['PVDescription']?></td>
             </tr>
         </table>
     </div>
